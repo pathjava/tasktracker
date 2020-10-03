28bf690c985dfd35b9d745398b150f088e70977b
Pathjava <severniyman@gmail.com>
2020-10-03 15:22:23 +0300

    update structure

--- src/main/java/ru/progwards/MainForTest.java
+++ src/main/java/ru/progwards/MainForTest.java
@@ -1,16 +1,16 @@
-package repository.dao;
+package ru.progwards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import repository.dao.impl.JsonHandlerImpl;
-import repository.dao.impl.TaskRepository;
-import repository.entity.Task;
-import service.converter.impl.ConverterTask;
-import service.vo.TaskModel;
-import util.types.Priority;
-import util.types.TaskType;
-import util.types.WorkflowStatus;
+import ru.progwards.repository.dao.impl.JsonHandlerTask;
+import ru.progwards.repository.dao.impl.TaskRepository;
+import ru.progwards.repository.entity.TaskEntity;
+import ru.progwards.service.converter.impl.ConverterTask;
+import ru.progwards.service.vo.Task;
+import ru.progwards.util.types.Priority;
+import ru.progwards.util.types.TaskType;
+import ru.progwards.util.types.WorkflowStatus;
 
 import java.time.ZonedDateTime;
 
@@ -18,16 +18,16 @@ import java.time.ZonedDateTime;
 public class MainForTest {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(MainForTest.class, args);
-        JsonHandlerImpl jsonHandler = context.getBean(JsonHandlerImpl.class);
+        JsonHandlerTask jsonHandler = context.getBean(JsonHandlerTask.class);
         TaskRepository taskRepository = context.getBean(TaskRepository.class);
         ConverterTask converterTask = context.getBean(ConverterTask.class);
 
-        jsonHandler.tasks.put(1L, new Task(1L, "task1", "description1", TaskType.BUG, Priority.MAJOR,
+        jsonHandler.tasks.put(1L, new TaskEntity(1L, "task1", "description1", TaskType.BUG, Priority.MAJOR,
                 001L, 003L,
                 ZonedDateTime.now().toEpochSecond(), ZonedDateTime.now().plusDays(1).toEpochSecond(),
                 100, 0005L, "STR_CODE_TTT", WorkflowStatus.NEW, "new_version",
                 123456L, 123456L, 123456L));
-        jsonHandler.tasks.put(2L, new Task(2L, "task2", "description2", TaskType.EPIC, Priority.CRITICAL,
+        jsonHandler.tasks.put(2L, new TaskEntity(2L, "task2", "description2", TaskType.EPIC, Priority.CRITICAL,
                 002L, 007L,
                 ZonedDateTime.now().plusDays(1).toEpochSecond(), ZonedDateTime.now().plusDays(3).toEpochSecond(),
                 101, 0006L, "STR_CODE_RRR", WorkflowStatus.IN_PROGRESS, "second_version",
@@ -40,7 +40,7 @@ public class MainForTest {
 
 //        taskRepository.delete(1L);
 
-        TaskModel taskModel = converterTask.convertTo(taskRepository.get(1L));
+        Task taskModel = converterTask.convertTo(taskRepository.get(1L));
         System.out.println(taskModel);
     }
 }
