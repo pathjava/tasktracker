1d685ec64d0cc05ad72d348c14f849af2cbc9481
Pathjava <severniyman@gmail.com>
2020-10-10 10:16:30 +0300

    updated

--- src/main/java/ru/progwards/tasktracker/service/api/ChangeTaskService.java
+++ src/main/java/ru/progwards/tasktracker/service/api/ChangeTaskService.java
@@ -1,6 +1,4 @@
 package ru.progwards.tasktracker.service.api;
-import ru.progwards.tasktracker.service.vo.Task;
-import ru.progwards.tasktracker.util.types.Status;
 
 public interface ChangeTaskService {
   
