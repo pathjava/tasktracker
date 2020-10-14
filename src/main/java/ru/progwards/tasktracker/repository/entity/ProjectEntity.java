2e39a981fbbbb6006f9d592aaf6852f8d88a348a
pavliggs <51763058+pavliggs@users.noreply.github.com>
2020-10-15 02:21:57 +0300

    create branch

--- src/main/java/ru/progwards/tasktracker/repository/entity/ProjectEntity.java
+++ src/main/java/ru/progwards/tasktracker/repository/entity/ProjectEntity.java
@@ -1,4 +1,45 @@
 package ru.progwards.tasktracker.repository.entity;
 
-public class ProjectEntity {
+import java.util.Arrays;
+
+public class ProjectEntity {private final long id;
+    private final String name;
+    private final String description;
+    private final String prefix;
+    private final long managerUserId;
+
+    public ProjectEntity(long id, String name, String description, long managerUserId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.prefix = getPrefix(name);
+        this.managerUserId = managerUserId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public long getManagerUserId() {
+        return managerUserId;
+    }
+
+    public static String getPrefix(String name) {
+        String[] items = name.split("\\s+");
+        StringBuilder stringBuilder = new StringBuilder();
+        Arrays.stream(items).forEach(e -> stringBuilder.append(e.substring(0, 1).toUpperCase()));
+        return stringBuilder.toString();
+    }
 }
--- src/main/java/ru/progwards/tasktracker/service/vo/Project.java
+++ src/main/java/ru/progwards/tasktracker/service/vo/Project.java
@@ -1,14 +1,31 @@
 package ru.progwards.tasktracker.service.vo;
 
-import java.time.ZonedDateTime;
-import java.util.List;
-
 public class Project {
-  
-    private List<Task> taskModels;
-  
-    private Long id;
-  
-    private ZonedDateTime created;
+    private final long id;
+    private final String name;
+    private final String description;
+    private final long managerUserId;
 
+    public Project(long id, String name, String description, long managerUserId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.managerUserId = managerUserId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long getManagerUserId() {
+        return managerUserId;
+    }
 }
