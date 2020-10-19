dd28ea4f5860e4f324f66c0254cf0508e956baa7
Pathjava <severniyman@gmail.com>
2020-10-19 17:40:10 +0300

    updated dto

--- src/main/java/ru/progwards/tasktracker/controller/dto/TaskDto.java
+++ src/main/java/ru/progwards/tasktracker/controller/dto/TaskDto.java
@@ -1,4 +1,38 @@
 package ru.progwards.tasktracker.controller.dto;
 
 public class TaskDto {
+
+    private Long id;
+    private String code;
+    private String name;
+
+    public TaskDto(Long id, String code, String name) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
