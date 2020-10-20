d7a132b63bb58f0da67d68bd5aa9b0dc002d11ff
Pathjava <severniyman@gmail.com>
2020-10-20 12:08:00 +0300

    updated tests

---
+++ src/test/java/ru/progwards/tasktracker/controller/converter/impl/TaskDtoConverterTest.java
@@ -0,0 +1,22 @@
+package ru.progwards.tasktracker.controller.converter.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TaskDtoConverterTest {
+
+    @Autowired
+    private TaskDtoConverter dtoConverter;
+
+    @Test
+    void toModel() {
+    }
+
+    @Test
+    void toDto() {
+    }
+}
