60486616cb0972487134750bf77b5ee5f8133124
Pathjava <severniyman@gmail.com>
2020-10-22 12:37:31 +0300

    updated tests

---
+++ src/test/java/ru/progwards/tasktracker/controller/converter/impl/TaskDtoFullConverterTest.java
@@ -0,0 +1,68 @@
+package ru.progwards.tasktracker.controller.converter.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.progwards.tasktracker.controller.dto.TaskDtoFull;
+import ru.progwards.tasktracker.service.vo.Task;
+import ru.progwards.tasktracker.service.vo.User;
+import ru.progwards.tasktracker.util.types.TaskPriority;
+import ru.progwards.tasktracker.util.types.TaskType;
+import ru.progwards.tasktracker.util.types.WorkFlowStatus;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+class TaskDtoFullConverterTest {
+
+    @Autowired
+    private TaskDtoFullConverter dtoFullConverter;
+
+    @Test
+    void toModel_return_Null() {
+        Task task = dtoFullConverter.toModel(null);
+
+        assertNull(task);
+    }
+
+    @Test
+    void toModel_return_Not_Null() {
+        Task task = dtoFullConverter.toModel(
+                new TaskDtoFull(1L, "TT1", "Test task 1 TEST", "Description task 1",
+                        TaskType.BUG, TaskPriority.MAJOR, 11L, new User(11L), new User(11L),
+                        ZonedDateTime.now(), ZonedDateTime.now().plusDays(1),
+                        new WorkFlowStatus(11L),
+                        Duration.ofDays(3), Duration.ofDays(1), Duration.ofDays(2),
+                        new ArrayList<>(), new ArrayList<>(), new ArrayList<>())
+        );
+
+        assertThat(task, is(notNullValue()));
+    }
+
+    @Test
+    void toDto_return_Null() {
+        TaskDtoFull task = dtoFullConverter.toDto(null);
+
+        assertNull(task);
+    }
+
+    @Test
+    void toDto_return_Not_Null() {
+        TaskDtoFull task = dtoFullConverter.toDto(
+                new Task(1L, "TT1", "Test task 1 TEST", "Description task 1",
+                        TaskType.BUG, TaskPriority.MAJOR, 11L, new User(11L), new User(11L),
+                        ZonedDateTime.now(), ZonedDateTime.now().plusDays(1),
+                        new WorkFlowStatus(11L),
+                        Duration.ofDays(3), Duration.ofDays(1), Duration.ofDays(2),
+                        new ArrayList<>(), new ArrayList<>(), new ArrayList<>())
+        );
+
+        assertThat(task, is(notNullValue()));
+    }
+}
