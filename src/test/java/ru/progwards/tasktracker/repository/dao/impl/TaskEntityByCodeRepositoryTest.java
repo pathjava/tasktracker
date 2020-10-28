c37b89b37a954943c9e78e0d56c436bc15022a00
Pathjava <severniyman@gmail.com>
2020-10-28 18:51:44 +0300

    updated tests

---
+++ src/test/java/ru/progwards/tasktracker/repository/dao/impl/TaskEntityByCodeRepositoryTest.java
@@ -0,0 +1,44 @@
+package ru.progwards.tasktracker.repository.dao.impl;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.progwards.tasktracker.repository.dao.Repository;
+import ru.progwards.tasktracker.repository.entity.TaskEntity;
+import ru.progwards.tasktracker.service.vo.User;
+import ru.progwards.tasktracker.util.types.TaskPriority;
+import ru.progwards.tasktracker.util.types.TaskType;
+import ru.progwards.tasktracker.util.types.WorkFlowStatus;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class TaskEntityByCodeRepositoryTest {
+
+    @Mock
+    private Repository<String, TaskEntity> repository;
+
+    @Test
+    void get() {
+        when(repository.get(anyString())).thenReturn(
+                new TaskEntity(1L, "TT1-1", "Test task 1 TEST", "Description task 1",
+                        TaskType.BUG, TaskPriority.MAJOR, 11L, new User(11L), new User(11L),
+                        ZonedDateTime.now().toEpochSecond(), ZonedDateTime.now().plusDays(1).toEpochSecond(),
+                        new WorkFlowStatus(11L),
+                        Duration.ofDays(3).toSeconds(), Duration.ofDays(1).toSeconds(), Duration.ofDays(2).toSeconds(),
+                        new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), false)
+        );
+
+        TaskEntity taskEntity = repository.get("TT1-1");
+
+        assertNotNull(taskEntity);
+        assertEquals("Test task 1 TEST", taskEntity.getName());
+    }
+}
