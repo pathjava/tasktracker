a7a88d95b6873897c99fb1d2590781e62ac5ba19
sidnet1964 <z112358Y>
2020-12-02 18:40:36 +0300

    Работа с классом User, версия 4

--- src/main/java/ru/progwards/tasktracker/controller/converter/impl/UserDtoPreviewConverter.java
+++ src/main/java/ru/progwards/tasktracker/controller/converter/impl/UserDtoPreviewConverter.java
@@ -5,6 +5,13 @@ import ru.progwards.tasktracker.controller.converter.Converter;
 import ru.progwards.tasktracker.controller.dto.UserDtoPreview;
 import ru.progwards.tasktracker.service.vo.User;
 
+/**
+ * Преобразование valueObject <-> dto
+ *
+ * User <-> UserDtoFull
+ *
+ * @author Aleksandr Sidelnikov
+ */
 @Component
 public class UserDtoPreviewConverter implements Converter<User, UserDtoPreview> {
     @Override
