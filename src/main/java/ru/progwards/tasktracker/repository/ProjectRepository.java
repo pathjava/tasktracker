f4521e6c5ddfa6a40042f4f301a4072814528d21
Oleg <javapath@yandex.ru>
2021-01-04 15:35:22 +0300

    updated Project Prefix Validator

--- src/main/java/ru/progwards/tasktracker/repository/ProjectRepository.java
+++ src/main/java/ru/progwards/tasktracker/repository/ProjectRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.progwards.tasktracker.model.Project;
 
+import java.util.Optional;
+
 /**
  * @author Pavel Khovaylo
  */
@@ -20,4 +22,11 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
      */
     boolean existsProjectByPrefix(String prefix);
 
+    /**
+     * Метод получения проекта по префиксу проекта
+     *
+     * @param prefix префикс проекта
+     * @return проект
+     */
+    Optional<Project> findByPrefix(String prefix);
 }
--- src/main/java/ru/progwards/tasktracker/util/validator/ProjectPrefixValidator.java
+++ src/main/java/ru/progwards/tasktracker/util/validator/ProjectPrefixValidator.java
@@ -4,6 +4,8 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.progwards.tasktracker.dto.ProjectDtoFull;
+import ru.progwards.tasktracker.model.Project;
 import ru.progwards.tasktracker.repository.ProjectRepository;
 import ru.progwards.tasktracker.util.validator.annotation.PrefixValid;
 
@@ -11,13 +13,13 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 /**
- * Валидатор для проверки существования prefix в БД при создании проекта
+ * Валидатор для проверки существования prefix в БД при создании и обновлении проекта
  *
  * @author Oleg Kiselev
  */
 @Service
 @RequiredArgsConstructor(onConstructor_ = {@Autowired, @NonNull})
-public class ProjectPrefixValidator implements ConstraintValidator<PrefixValid, String> {
+public class ProjectPrefixValidator implements ConstraintValidator<PrefixValid, ProjectDtoFull> {
 
     private final ProjectRepository projectRepository;
 
@@ -26,12 +28,22 @@ public class ProjectPrefixValidator implements ConstraintValidator<PrefixValid, 
     }
 
     @Override
-    public boolean isValid(String prefix, ConstraintValidatorContext context) {
-        if (projectRepository.existsProjectByPrefix(prefix)) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(prefix + " already exists")
-                    .addConstraintViolation();
-            return false;
+    public boolean isValid(ProjectDtoFull projectDto, ConstraintValidatorContext context) {
+        if (projectDto.getId() == null) {
+            if (projectRepository.existsProjectByPrefix(projectDto.getPrefix())) {
+                context.disableDefaultConstraintViolation();
+                context.buildConstraintViolationWithTemplate(projectDto.getPrefix() + " already exists")
+                        .addConstraintViolation();
+                return false;
+            }
+        } else {
+            Project project = projectRepository.findByPrefix(projectDto.getPrefix()).orElse(null);
+            if (project != null && !project.getId().equals(projectDto.getId())) {
+                context.disableDefaultConstraintViolation();
+                context.buildConstraintViolationWithTemplate(projectDto.getPrefix() + " already exists")
+                        .addConstraintViolation();
+                return false;
+            }
         }
         return true;
     }
