4fca01eb3a03ecb8b18f8d7d1fe0571c1721ce3b
Grigory Lobkov <gg81@ya.ru>
2021-01-09 22:37:57 +0500

    merge

--- src/main/java/ru/progwards/tasktracker/dto/TaskNoteDtoPreview.java
+++ src/main/java/ru/progwards/tasktracker/dto/TaskNoteDtoPreview.java
@@ -18,4 +18,5 @@ public class TaskNoteDtoPreview {
     private User author;
     private User updater;
     private String comment;
+
 }
--- src/main/java/ru/progwards/tasktracker/service/impl/TaskTypeService.java
+++ src/main/java/ru/progwards/tasktracker/service/impl/TaskTypeService.java
@@ -145,8 +145,8 @@ public class TaskTypeService implements CreateService<TaskType>, GetService<Long
     @Override
     public void createFromTemplate(Object... args) {
         if(args.length!=2) throw new OperationIsNotPossibleException("TaskType.createFromTemplate: 2 arguments expected");
-        if(args[0] instanceof Project) throw new OperationIsNotPossibleException("TaskType.createFromTemplate: argument 0 must be Project");
-        if(args[1] instanceof WorkFlow) throw new OperationIsNotPossibleException("TaskType.createFromTemplate: argument 1 must be WorkFlow");
+        if(!(args[0] instanceof Project)) throw new OperationIsNotPossibleException("TaskType.createFromTemplate: argument 0 must be Project");
+        if(!(args[1] instanceof WorkFlow)) throw new OperationIsNotPossibleException("TaskType.createFromTemplate: argument 1 must be WorkFlow");
 
         List<String> names = List.of("Task", "Bug", "Epic");
         List<TaskType> tts = new ArrayList<>(names.size());
