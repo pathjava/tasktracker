2704b296deb037d5aaa131f69c6796f4a57db1d3
Oleg <javapath@yandex.ru>
2021-01-27 12:00:40 +0300

    updated - SecurityConfig

--- src/main/java/ru/progwards/tasktracker/security/SecurityConfig.java
+++ src/main/java/ru/progwards/tasktracker/security/SecurityConfig.java
@@ -12,7 +12,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic();
     }
 
 }
