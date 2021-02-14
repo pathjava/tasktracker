6e8393ebe503d3f7ea7453f42fb3972b4398774e
Ikast <Ikastru@yandex.ru>
2021-02-14 15:04:49 +0300

    Ikast Profiler Modified

--- src/main/java/ru/progwards/tasktracker/util/profiler/ProfilerAspect.java
+++ src/main/java/ru/progwards/tasktracker/util/profiler/ProfilerAspect.java
@@ -2,10 +2,11 @@ package ru.progwards.tasktracker.util.profiler;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Oleg Kiselev
@@ -21,8 +22,14 @@ public class ProfilerAspect {
     public void profilerPointcut() {
     }
 
+    private Map<String, Long> durationMap = new HashMap<>();
+    private Map<String, Long> countMap = new HashMap<>();
+    static long interval = System.currentTimeMillis();
+   // private final Logger loggerProfiler = LoggerFactory.getLogger(ProfilerAspect.class);
+
     @Around("profilerPointcut()")
     public Object profilerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
         StopWatch stopWatch = new StopWatch(joinPoint.toString());
 
         try {
@@ -30,7 +37,24 @@ public class ProfilerAspect {
             return joinPoint.proceed();
         } finally {
             stopWatch.stop();
-            System.out.println("Method execution time: " + stopWatch.getTotalTimeMillis() + " milliseconds");
+            //System.out.println("Method execution time: " + stopWatch.getTotalTimeMillis() + " milliseconds");
+            if (durationMap.containsKey(joinPoint.toString())){
+                durationMap.put(joinPoint.toString(), durationMap.get(joinPoint.toString())+stopWatch.getTotalTimeMillis());
+                countMap.put(joinPoint.toString(), countMap.get(joinPoint.toString())+1L);
+            } else{
+                durationMap.put(joinPoint.toString(), stopWatch.getTotalTimeMillis());
+                countMap.put(joinPoint.toString(), 1L);
+            }
+            printProfiler();
+        }
+    }
+
+    public void printProfiler(){
+        if((System.currentTimeMillis() - interval) > 3600000){
+            for(Map.Entry<String, Long> entry : durationMap.entrySet()) {
+                System.out.println(entry.getKey() + ": " +  entry.getValue() + "ms" + countMap.get(entry.getKey()) + " execs.");
+            }
+            interval=System.currentTimeMillis();
         }
     }
 }
